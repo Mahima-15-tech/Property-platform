5e8b87063dc7ea9122dfa9fdaa3d534ead886cc3
Jitendra Singh Bhadauria <jitendrasingh@Jitendras-MacBook-Air.local>
2026-03-28 17:53:38 +0530

    fix cors path error

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const app = express();
 
 
 
+
+
 app.use(cors({
   origin: [
     "http://localhost:5173",
@@ -19,6 +21,9 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
+// ❌ REMOVE THIS
+// app.options("*", cors());
 
 // 🔥 IMPORTANT (preflight fix)
 app.options("*", cors());
