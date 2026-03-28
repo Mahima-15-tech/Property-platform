7d48471fcc134e3451c4caf95577039250d53a43
Jitendra Singh Bhadauria <jitendrasingh@Jitendras-MacBook-Air.local>
2026-03-28 17:31:35 +0530

    fix cors final

--- server/server.js
+++ server/server.js
@@ -8,21 +8,15 @@ connectDB();
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://property-platform-six.vercel.app"
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: [
+    "http://localhost:5173",
+    "https://property-platform-six.vercel.app"
+  ],
   credentials: true
 }));
+
+
 app.use(express.json());
 
 
